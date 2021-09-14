9429f0128c7ed8ddf4c83872373cb1a3c51907ac
Fagner Lisboa <fnlisboa@gmail.com>
2021-09-14 20:30:42 -0300

    olha ai Zé

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  formBuilder: any;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  loginForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+        userName: ['', Validators.required],
+        password: ['', Validators.required]
+    });
+ }
 }
