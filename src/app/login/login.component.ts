c610fe94e2258ec46f58f5d6664bf5eee0ac46db
Fagner Lisboa <fnlisboa@gmail.com>
2021-10-20 22:36:12 -0300

    Ajustes Grid

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -44,6 +44,23 @@ export class LoginComponent implements OnInit {
         control.markAllAsTouched
       })
     }
+    const visibilityToggle = document.querySelector('.visibility');
+
+    const input = document.querySelector('.input-container input');
+
+    var password = true;
+
+    visibilityToggle.addEventListener('click', function () {
+      if (password) {
+        input.setAttribute('type', 'text');
+        visibilityToggle.innerHTML = 'visibility';
+      } else {
+        input.setAttribute('type', 'password');
+        visibilityToggle.innerHTML = 'visibility_off';
+      }
+      password = !password;
+      
+    });
   }
   ChecksValidTouched(campo: string | (string | number)[]) {
     return !this.loginForm.get(campo).valid && this.loginForm.get(campo).touched;
@@ -59,4 +76,5 @@ export class LoginComponent implements OnInit {
   get password() {
     return this.loginForm.get('password')
   }
+
 }
